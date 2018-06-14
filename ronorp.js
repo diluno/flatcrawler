228d99c19fc2ace896bbce78d0260d7362f07fe3
Samuel Alder <samuel.alder@gmail.com>
2018-06-14 10:58:22 +0200

    adjusted ronorp url

--- ronorp.js
+++ ronorp.js
@@ -1,6 +1,6 @@
 module.exports = {
   db: 'ronorp',
-  uri: 'https://www.ronorp.net/zuerich/immobilien/wohnung-zuerich.1219?hash=7e27305410eb19bd251d207da800cf29&fdata%5Brons_filter%5D=&cmd=update&record_id=&fdata%5Badvert_type%5D=angebot&fdata%5Bsp_realty_type%5D=rent&fdata%5Bsp_realty_stadt_agglo%5D=stadt&fdata%5Bsp_realty_contract_limited%5D=N&fdata%5Bplz%5D=&fdata%5Bsp_realty_plz%5D=&page=1&adverts_filter=1&fdata%5Bsearch%5D=',
+  uri: 'https://www.ronorp.net/zuerich/immobilien/wohnung-zuerich.1219?hash=7e27305410eb19bd251d207da800cf29&fdata%5Brons_filter%5D=&cmd=update&record_id=&fdata%5Badvert_type%5D=angebot&fdata%5Bsp_realty_type%5D=rent&fdata%5Bsp_realty_stadt_agglo%5D=stadt&fdata%5Bplz%5D=&fdata%5Bsp_realty_plz%5D=&page=1&adverts_filter=1&fdata%5Bsearch%5D=',
   parse: ($, makeHash) => {
     let flats = [];
     const entries = $('.short_advert');
